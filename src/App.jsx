4f2c3e3b237420a10bef7ca829c2d8112503d3d9
almamani <al.mamani@gmail.com>
2025-05-20 12:57:10 -0300

    Iniciando proyecto React Bluuweb

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Clase0 from "./views/Clase0";
+
+function App() {
+  return (
+    <>
+      <Clase0 />
+    </>
+  );
+}
+
+export default App;
---
+++ src/views/Clase0.jsx
@@ -0,0 +1,10 @@
+const Clase0 = () => {
+  return (
+    <div className="contenedor contenedor__centrado">
+      <h1>Tutorial React</h1>
+      <h2>Clase 0 - Tema</h2>
+    </div>
+  );
+};
+
+export default Clase0;
